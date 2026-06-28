d2cc2f1d2ef63275984a118a1bb4cfd72ddb52cd
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-06-28 20:22:16 +0300

    Revisiting the server setup

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ const wss = new webSocket.Server({port:process.env.PORT})
 
 console.log(`websocket server runing on ws://localhost:${process.env.PORT}`)
 
-
+// on connection
 wss.on("connection", (ws)=>{
     console.log("New client connected")
 
-
+// send welcome message to the client
     ws.send("welcome to the websocket")
 
+    // 
     ws.on("message", (message)=>{
         console.log(`Received: ${message}`)
 
